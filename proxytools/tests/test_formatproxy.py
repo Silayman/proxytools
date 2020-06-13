b644f189dd1cff86a0e19b19c030d6d51fade25c
Silayman Nagi <silayman.nagi@gmail.com>
2020-06-12 22:06:59 -0400

    Added test for proxies with user:pass authentication

--- proxytools/tests/test_formatproxy.py
+++ proxytools/tests/test_formatproxy.py
@@ -2,9 +2,13 @@ import unittest
 import proxytools
 
 class TestFormatProxy(unittest.TestCase):
-    def test_formatproxy(self):
+    def test_formatproxy_noauth(self):
         returned_output = proxytools.formatproxy("162.10.10.10:3128").format_request()
         expected_output = {'http': 'http://162.10.10.10:3128', 'https': 'https://162.10.10.10:3128'}
+        self.assertEqual(returned_output, expected_output)
+    def test_formatproxy_auth(self):
+        returned_output = proxytools.formatproxy("162.10.10.10:3128:username:password").format_request()
+        expected_output = {'http': 'http://username:password@162.10.10.10:3128', 'https': 'https://username:password@162.10.10.10:3128'}
         self.assertEqual(returned_output, expected_output)
 
 
